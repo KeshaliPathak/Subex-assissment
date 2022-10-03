ec8fe281b13bbd308fd340feb57247d169c2dfee
Keshali Pathak <106054299+KeshaliPathak@users.noreply.github.com>
2022-10-03 21:42:09 +0530

    tables programs

---
+++ Tables.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		   {
+		        System.out.println("Enter a number");
+		Scanner myObj = new Scanner(System.in);
+		 
+        int N = myObj.nextInt();
+        // int N =2;
+        for(int i=1;i<=N;i++)
+        {
+            System.out.println("\n");
+        for (int j = 1; j <= 10; j++) 
+        {
+            System.out.println(i + " * " + j + " = "  + i * j );
+        }
+        
+    }
+}
+	}
+}
